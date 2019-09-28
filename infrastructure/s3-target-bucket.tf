6b3f4c2630e3a4c3bd7fa269badc85d7a8202011
Przemyslaw S <przmk01@gmail.com>
2019-09-28 21:28:39 +0200

    S3 target bucket definition formatted

--- infrastructure/s3-target-bucket.tf
+++ infrastructure/s3-target-bucket.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "target-bucket" {
-    bucket = "psp-file-duplicator-target"
-    acl = "private"
-    force_destroy = "true"
+  bucket        = "psp-file-duplicator-target"
+  acl           = "private"
+  force_destroy = "true"
 
-    tags = {
-        Description = "The target bucket for the copy-file lambda"
-    }
+  tags = {
+    Description = "The target bucket for the copy-file lambda"
+  }
 }
