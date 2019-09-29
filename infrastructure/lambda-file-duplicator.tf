cfe1208e74ffb119d6dc6eaaea8cb60b6e3f9e92
Przemyslaw S <przmk01@gmail.com>
2019-09-29 18:53:33 +0200

    Arn replaced with function_name in lambda permission defition

--- infrastructure/lambda-file-duplicator.tf
+++ infrastructure/lambda-file-duplicator.tf
@@ -22,7 +22,7 @@ resource "aws_lambda_function" "file-duplicator-lambda" {
 resource "aws_lambda_permission" "allow-source-bucket-event" {
   statement_id  = "AllowExecutionFromS3Bucket"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.file-duplicator-lambda.arn}"
+  function_name = "${aws_lambda_function.file-duplicator-lambda.function_name}"
   principal     = "s3.amazonaws.com"
   source_arn    = "${aws_s3_bucket.source-bucket.arn}"
 }
