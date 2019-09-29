892f061d0a582a4cebca969dff2e5ed97992542d
Przemyslaw S <przmk01@gmail.com>
2019-09-29 17:29:00 +0200

    Lambda permission to trigger via S3 event added

--- infrastructure/lambda-file-duplicator.tf
+++ infrastructure/lambda-file-duplicator.tf
@@ -19,6 +19,14 @@ resource "aws_lambda_function" "file-duplicator-lambda" {
   }
 }
 
+resource "aws_lambda_permission" "allow-source-bucket-event" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.file-duplicator-lambda.arn}"
+  principal     = "s3.amazonaws.com"
+  source_arn    = "${aws_s3_bucket.source-bucket.arn}"
+}
+
 locals {
   function-zip-name = "file-duplicator-lambda.zip"
 }
